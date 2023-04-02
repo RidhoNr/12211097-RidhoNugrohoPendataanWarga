a05c01bf848f2cbb5b0b412ff2fa97b2e632022a
RidhoNr <ridhonugroho890@gmail.com>
2023-04-02 16:18:48 +0700

    pendataan warga

---
+++ aplikasipendataanwarga2/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:aplikasipendataanwarga2/login_view.dart';
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MaterialApp(
+    home : LoginView()
+  )); 
+}
